912f1dba8e1c3e16502826cb0c5a8d3d7869559e
ranaaalihaider <135860810+ranaaalihaider@users.noreply.github.com>
2024-01-18 16:39:56 +0500

    C++ Code 1st Samester
    C++ Code 1st Samester

---
+++ Apna College C++/2-3-if-else/2-3-if-else.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+int main() {
+
+    int savings;
+    cin >> savings;
+
+    if (savings > 5000) {
+        cout << "Neha\n";
+    } else {
+        cout << "Rashmi\n";
+    }
+
+    return 0;
+}
